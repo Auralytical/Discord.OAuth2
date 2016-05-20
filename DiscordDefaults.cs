924919dd3855a61f3a0ec81f941b7f6b2273c875
RogueException <rogueexception@gmail.com>
2016-05-20 01:57:55 -0300

    Added a few docstrings

--- DiscordDefaults.cs
+++ DiscordDefaults.cs
@@ -1,13 +1,16 @@
 ﻿namespace Discord.OAuth2
 {
+    /// <summary> A collection of default values used by DiscordMiddleware. </summary>
     public static class DiscordDefaults
     {
+        /// <summary> The default authentication scheme. </summary>
         public const string AuthenticationScheme = "Discord";
 
+        /// <summary> The default oauth authentication endpoint for Discord. </summary>
         public static readonly string AuthorizationEndpoint = "https://discordapp.com/api/oauth2/authorize";
-
+        /// <summary> The default oauth token endpoint for Discord. </summary>
         public static readonly string TokenEndpoint = "https://discordapp.com/api/oauth2/token";
-
+        /// <summary> The default user information endpoint used to generate claims. </summary>
         public static readonly string UserInformationEndpoint = "https://discordapp.com/api/users/@me";
     }
 }
